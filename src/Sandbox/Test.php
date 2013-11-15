7b0334b384905dfe0d1289f3f612a2936061ba7d
pokoot <pokoot@gmail.com>
2013-11-15 22:26:46 +0800

    test

--- src/Sandbox/Test.php
+++ src/Sandbox/Test.php
@@ -12,7 +12,16 @@ class Test
     public static function hello()
     {
         return 'This is version 0.1.1';
+    }
 
+    public static function shoot()
+    {
+        return 'This is version 0.1.2';
+    }
+
+    public static function oh_my_sandbox()
+    {
+        return 'Fast forwarding?';
     }
 
 }
